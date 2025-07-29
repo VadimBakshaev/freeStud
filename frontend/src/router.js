f7cb73e38dff6e56b9876f5ba5fb95610e715d73
Vadim <vadimm@citydom.ru>
2025-07-29 13:07:13 +0300

    add login and signup pages

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -6,6 +6,7 @@ export class Router {
     constructor() {
         this.titlePageEl = document.getElementById('title');
         this.contentPageEl = document.getElementById('content');
+        this.adminlteStyleEl = document.getElementById('adminlte_style');
         this.initEvents();
         this.routes = [
             {
@@ -32,8 +33,15 @@ export class Router {
                 filePathTemplate: '/templates/login.html',
                 useLayout: false,
                 load() {
+                    document.body.classList.add('login-page');
+                    document.body.style.height = '100vh';
                     new Login();
-                }
+                },
+                unload(){
+                    document.body.classList.remove('login-page');
+                    document.body.style.height = 'auto';
+                },
+                styles: ['icheck-bootstrap.min.css']
             },
             {
                 route: '/sign-up',
@@ -41,18 +49,64 @@ export class Router {
                 filePathTemplate: '/templates/sign-up.html',
                 useLayout: false,
                 load() {
+                    document.body.classList.add('register-page');
+                    document.body.style.height = '100vh';
                     new SignUp();
-                }
+                },
+                unload(){
+                    document.body.classList.remove('register-page');
+                    document.body.style.height = 'auto';
+                },
+                styles: ['icheck-bootstrap.min.css']
             },
         ];
     };
     initEvents() {
         window.addEventListener('DOMContentLoaded', this.activateRoute.bind(this));
         window.addEventListener('popstate', this.activateRoute.bind(this));
-    }
-    async activateRoute() {
+        document.addEventListener('click', this.openNewRoute.bind(this));
+    };
+    openNewRoute(e) {
+        let element = null;
+        if (e.target.nodeName === 'A') {
+            element = e.target;
+        } else if (e.target.parentNode.nodeName === 'A') {
+            element = e.target.parentNode;
+        };
+        if (element) {
+            e.preventDefault();
+            const url = element.href.replace(location.origin, '');
+            if (!url || url === '/#' || url.startsWith('javascript:void(0)')) {
+                return;
+            };
+            const currentRoute = location.pathname;
+            history.pushState({}, '', url);
+            this.activateRoute(null, currentRoute);
+        };
+    };
+    async activateRoute(e, oldRoute = null) {
+        if(oldRoute){
+            const currentRoute = this.routes.find(item => item.route === oldRoute);
+            if (currentRoute.styles && currentRoute.styles.length > 0) {
+                currentRoute.styles.forEach(style => {
+                    document.querySelector(`link[href='/css/${style}']`).remove();
+                });
+            };
+            if (currentRoute.unload && typeof currentRoute.unload === 'function') {
+                currentRoute.unload();
+            };
+        };
         const newRoute = this.routes.find(item => item.route === location.pathname);
         if (newRoute) {
+            if (newRoute.styles && newRoute.styles.length > 0) {
+                newRoute.styles.forEach(style => {
+                    const linkEl = document.createElement('link');
+                    linkEl.rel = 'stylesheet';
+                    linkEl.href = '/css/' + style;
+                    this.adminlteStyleEl
+                    document.head.insertBefore(linkEl, this.adminlteStyleEl);
+                });
+            };
             if (newRoute.title) {
                 this.titlePageEl.innerText = newRoute.title + ' | Freelance Studio';
             };
@@ -77,8 +131,9 @@ export class Router {
             };
         } else {
             console.log('No route found');
-            location.href = '/404';
-        }
+            history.pushState({}, '', '/404');
+            this.activateRoute();
+        };
     };
     async #constructTemplate(templateElement, newRoute) {
         templateElement.innerHTML = await fetch(newRoute)
